fe533933d807a643c4206bdb58842beb55152978
Romaric Ano <jeannoelromaricano@gmail.com>
2020-06-05 23:55:38 +0000

    update

--- myteam/urls.py
+++ myteam/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 
 from myteam import views
+# from blog import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('', views.schedule, name='schedule'),
-    path('', views.player, name='player'),
-    path('', views.team, name='team'),
+    path('schedule', views.schedule, name='schedule'),
+    path('player', views.player, name='player'),
+    path('team', views.team, name='team'),
+    path('league', views.league, name='league'),
+    path('contact', views.contact, name='contact'),
+    # path('blog', views.blog, name='blog'),
 
 ]
--- myteam/views.py
+++ myteam/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/player-list-gallery.html')
+    return render(request, 'pages/home.html')
 
 def schedule(request):
     return render(request, 'pages/schedule-widget-style.html')
@@ -14,8 +14,12 @@ def schedule(request):
 def player(request):
     return render(request, 'pages/player-list-gallery.html')
 
-
 def team(request):
-    return render(request, 'pages/about-our-team.html')
+    return render(request, 'pages/liverpool.html')
 
+def league(request):
+    return render(request, 'pages/league-table.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
 # Create your views here.
