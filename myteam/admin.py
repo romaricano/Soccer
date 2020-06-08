abfde71249f643d58904573e6f8d6ac25d33ec48
Romaric Ano <jeannoelromaricano@gmail.com>
2020-06-08 12:43:20 +0000

    admin and models

--- myteam/admin.py
+++ myteam/admin.py
@@ -1,3 +1,259 @@
 from django.contrib import admin
+from myteam import models
+
 
 # Register your models here.
+
+
+class PosteAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+class LigueAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+class CoachAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+class MatchAdmin(admin.ModelAdmin):
+    list_display = (
+        'ligue',
+        'score',
+        'equipeA',
+        'equipeB',
+        # 'date',
+        'lieu',
+
+    )
+    # date_hierarchy = 'date'
+    list_filter = (
+        'score',
+        'ligue',
+        # 'date',
+    )
+    search_fields = (
+        'score',
+    )
+    list_per_page = 10
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'ligue',
+                # 'date',
+                'lieu',
+            ]
+        }),
+        ('Equipes', {
+            'fields': [
+                'equipeA',
+                'equipeB',
+            ]
+        }),
+        ('Resultat', {
+            'fields': [
+                'score',
+            ]
+        })
+    ]
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'nom',
+        'coach',
+        'points',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+        'coach',
+        'ligue',
+    )
+    search_fields = (
+        'nom',
+    )
+    list_per_page = 10
+
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'nom',
+                'ligue',
+                'coach',
+                'points',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+class StadeAdmin(admin.ModelAdmin):
+    list_display = (
+        'titre',
+        'team',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+        'team',
+    )
+    search_fields = (
+        'titre',
+    )
+    list_per_page = 10
+
+    fieldsets = [
+        ('Info', {
+            'fields': [
+                'titre',
+                'team',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = (
+        'nom',
+        'prenom',
+        'taille',
+        'poids',
+        'team',
+        'numero',
+        'nbr_buts',
+        'status',
+
+    )
+    list_filter = (
+        'status',
+        'team',
+        'numero',
+        'nbr_buts',
+        'poste',
+    )
+    search_fields = (
+        'nom',
+    )
+    list_per_page = 10
+
+    fieldsets = [
+        ('Info sur le joueur', {
+            'fields': [
+                'nom',
+                'prenom',
+                'taille',
+                'poids',
+            ]
+        }),
+        ('Equipe', {
+            'fields': [
+                'team',
+                'poste',
+                'numero',
+            ]
+        }),
+        ('Carrière', {
+            'fields': [
+                'nbr_buts',
+            ]
+        }),
+        ('Status et Activation', {
+            'fields': [
+                'status',
+            ]
+        })
+    ]
+
+
+
+def _register(model, admin_class):
+    admin.site.register(model, admin_class)
+
+
+_register(models.Poste, PosteAdmin)
+_register(models.Ligue, LigueAdmin)
+_register(models.Coach, CoachAdmin)
+_register(models.Match, MatchAdmin)
+_register(models.Team, TeamAdmin)
+_register(models.Stade, StadeAdmin)
+_register(models.Player, PlayerAdmin)
--- myteam/models.py
+++ myteam/models.py
@@ -1,3 +1,126 @@
 from django.db import models
+# import datetime
+# from django.utils import timezone
+
 
 # Create your models here.
+class Poste(models.Model):
+    titre = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Poste"
+        verbose_name_plural = "Postes"
+
+    def __str__(self):
+        return str(self.titre)
+
+
+class Ligue(models.Model):
+    titre = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Ligue'
+        verbose_name_plural = 'Ligues'
+
+    def __str__(self):
+        return str(self.titre)
+
+
+class Coach(models.Model):
+    titre = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Coach'
+        verbose_name_plural = 'Coachs'
+
+    def __str__(self):
+        return str(self.titre)
+
+
+class Team(models.Model):
+    nom = models.CharField(max_length=255)
+    # logo = models.ImageField(upload_to='restaurant/menu/photo', null=True, blank=True)
+    # ligue = models.ForeignKey(Ligue, on_delete=models.CASCADE, related_name='team_ligue')
+    ligue = models.ManyToManyField('Ligue', related_name='ligues')
+    coach = models.ForeignKey(Coach, on_delete=models.CASCADE, related_name='coachs')
+
+    points = models.IntegerField(default=0)
+
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+    def __str__(self):
+        return str(self.nom)
+
+
+class Match(models.Model):
+    ligue = models.ForeignKey('Ligue', on_delete=models.CASCADE, related_name=None)
+    score = models.CharField(max_length=6)
+    equipeA = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='match_teamA')
+    equipeB = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='match_teamB')
+    date = models.DateTimeField()
+
+    lieu = models.ForeignKey('Stade', on_delete=models.CASCADE, related_name='lieux')
+
+    class Meta:
+        verbose_name = "Match"
+        verbose_name_plural = "Matchs"
+
+    def __str__(self):
+        return str({}, '-', {}).format(self.equipeA, self.equipeB)
+
+    # @property
+    # def isPlayed(self):
+    #     return timezone.now - self.date <= 0
+
+    # @property
+    # def givePoint(self):
+    #     if self.isPlayed:
+
+
+class Player(models.Model):
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    taille = models.IntegerField()
+    poids = models.IntegerField()
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='teams')
+    poste = models.ManyToManyField('Poste', related_name='teams')
+    numero = models.IntegerField()
+
+    nbr_buts = models.IntegerField(default=0)
+
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Player'
+        verbose_name_plural = 'Players'
+
+    def __str__(self):
+        return str(self.nom)
+
+    @property
+    def getTeam(self):
+        return self.team.nom
+
+    @property
+    def getPoste(self):
+        return self.poste.filter(status=True)
+
+
+class Stade(models.Model):
+    titre = models.CharField(max_length=255)
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='stade_team')
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Stade'
+        verbose_name_plural = 'Stades'
+
+    def __str__(self):
+        return str(self.titre)
